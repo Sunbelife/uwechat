794275f8a50c861e4017d8fa17519b2328223305
sunbelife <Sunbelife@foxmail.com>
2017-07-28 00:46:12 +0800

    put the font file at Local
    Adjust some code, and make the font file in local.
    
    Signed-off-by: sunbelife <Sunbelife@foxmail.com>

--- uwechat.py
+++ uwechat.py
@@ -24,30 +24,24 @@ total = male + female + other
 
 # for Chinese support
 def getChineseFont():  
-    return FontProperties(fname='/System/Library/Fonts/PingFang.ttc')  
-
+    return FontProperties(fname='fonts/PingFang.ttc')  
 def draw_it(labels,quants):  
     plt.figure(figsize=(6,8))
     sizes = quants
     colors = ['blue','pink','green']
     explode = (0.05,0,0)
-
     patches,l_text,p_text = plt.pie(sizes,explode=explode,labels=labels,colors=colors,
                                 labeldistance = 1.1,autopct = '%3.1f%%',shadow = False,
                                 startangle = 90,pctdistance = 0.6)
-
     for t in l_text:
         t.set_size=(30)
     for t in p_text:
         t.set_size=(20)
-
     plt.title(u'微信好友性别分布',fontproperties=getChineseFont())
     plt.axis('equal')
     plt.legend()
     plt.show()
   
-labels   = [u'Males', u'Females', u'Others']
-  
-quants   = [float(male)/total * 100, float(female)/total * 100, float(other)/total * 100]   
-  
+labels = [u'Males', u'Females', u'Others']
+quants = [float(male)/total * 100, float(female)/total * 100, float(other)/total * 100]   
 draw_it(labels,quants) 
